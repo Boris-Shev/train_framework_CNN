e4d54793ff04405bf5d308579980caa2f5a1e93d
Boris <kialol458@gmail.com>
2024-12-11 16:31:20 +0300

    Little changes

--- data/get_data.py
+++ data/get_data.py
@@ -7,16 +7,16 @@ from torch.utils.data import random_split
 from functools import partial
 
 
-
 def get_dataloader( dataset: str = 'MNIST',
                     root: str = os.path.join('data', 'datasets'),
                     batch_size: int = 32,
                     num_workers: int = 0,
                     val_size: float = 0.2,
+                    test_size: float = 0e-10,
                     img_height: int = 28,
                     img_width: int = 28
                     ) -> DataLoader:
-    avaliable_datasets = ['MNIST', 'CIFAR10']
+    avaliable_datasets = ['MNIST', 'CIFAR10', 'EuroSAT']
     if dataset in avaliable_datasets:
         torch_dataset = getattr(torchvision.datasets, dataset)
     else:
@@ -29,9 +29,11 @@ def get_dataloader( dataset: str = 'MNIST',
         tfs.Resize((img_height, img_width))
     ])
 
-    train_dataset = torch_dataset(root, train=True,  transform=data_tfs, download=True)
-    train_dataset, val_dataset = random_split(train_dataset, [1 - val_size, val_size])
-    test_dataset  = torch_dataset(root, train=False, transform=data_tfs, download=True)
+    import ssl 
+    ssl._create_default_https_context = ssl._create_unverified_context
+    train_size = 1 - val_size - test_size
+    train_dataset = torch_dataset(root, transform=data_tfs, download=True)
+    train_dataset, val_dataset, test_dataset = random_split(train_dataset, [train_size, val_size, test_size])
 
     train_dataloader =  DataLoader(train_dataset, 
                                 batch_size=batch_size, 
@@ -43,4 +45,3 @@ def get_dataloader( dataset: str = 'MNIST',
                                 batch_size=batch_size, 
                                 num_workers=num_workers)
     return train_dataloader, val_dataloader, test_dataloader
-    
